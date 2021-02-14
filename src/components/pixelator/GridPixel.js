67bfbf6bc97b99ea547ff1f312be2d0ffec9d4e7
Theodorus Riyanto <theodorus_riyanto@yahoo.com>
2021-02-14 19:56:36 +0700

    cleaning up code, Ver 1.1.2

--- src/components/pixelator/GridPixel.js
+++ src/components/pixelator/GridPixel.js
@@ -2,20 +2,13 @@ import React from 'react';
 import {motion} from 'framer-motion';
 
 const GridPixel = ({pixel}) => {
-  // const props = useSpring({
-  //   config: {
-  //     duration: 1500
-  //   },
-  //   to: pixel%2 == 0 ? {backgroundColor: '#4a6274', rotateY: 0} : {backgroundColor: '#94acbf', rotateY: 180},
-  //   from: {backgroundColor: '#4a6274', rotateY: 0}
-  // })
 
   return (
     <motion.div className='grid-pixel' 
       initial={{backgroundColor: '#4a6274', rotateY: 0}} 
       animate={pixel%2 == 0 ? {backgroundColor: '#4a6274', rotateY: 0} : {backgroundColor: '#94acbf', rotateY: 180}}
       transition={{
-        duration: 2
+        duration: 1.5
       }}
     >
     </motion.div>
